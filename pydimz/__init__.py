66765c997c01f326accabe2a875065e42e8f0e69
BrandonPacewic <pacewicbrandon@gmail.com>
2024-01-14 06:59:57 -0800

    Works with other langugages now

--- pydimz/__init__.py
+++ pydimz/__init__.py
@@ -5,5 +5,5 @@
 pydimz - CLI tool for counting the number of lines of code in any given project.
 """
 
-__version__ = "0.1.dev2"
+__version__ = "0.1.dev3"
 __all__ = ["version"]
--- pydimz/__main__.py
+++ pydimz/__main__.py
@@ -6,40 +6,70 @@ pydimz - CLI tool for counting the number of lines of code in any given project.
 """
 
 import os
+import json
 import sys
 import token
 import tokenize
 
+from dataclasses import dataclass
 from tabulate import tabulate
-from typing import List
+from typing import List, Tuple
 
+MODULE_DIR = os.path.dirname(__file__)
 TOKEN_WHITELIST = [token.OP, token.NAME, token.NUMBER, token.STRING]
 
 
-def gen_dim_table(dir: str = os.getcwd()) -> List[List[str | int]]:
-    table: List[List[str | int]] = []
+@dataclass
+class FileData:
+    filename: str
+    line_count: int
+    token_count: int
+    token_per_line: float
+    language: str
+
+    def expand(self) -> List[str | int | float]:
+        attrs = vars(self)
+        return [x for x in attrs.values()]
+
+
+def gen_filetypes() -> Tuple[set, dict[str, str]]:
+    with open(f"{MODULE_DIR}/language_extensions.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    
+    return set([x for x in data.keys()]), data
+
+
+def gen_dim_table(dir: str = os.getcwd()) -> List[FileData]:
+    valid_extensions, language_type = gen_filetypes()
+    table: List[FileData] = []
 
     for path, _, files in os.walk(dir):
         for filename in files:
-            if not filename.endswith(".py"):
+            file_extension = f".{filename.split('.')[-1]}"
+
+            if not file_extension in valid_extensions:
                 continue
 
             filepath = os.path.join(path, filename)
             relfilepath = os.path.relpath(filepath, dir)
 
-            with tokenize.open(filepath) as file:
-                tokenized = tokenize.generate_tokens(file.readline)
-                tokens = [t for t in tokenized if t.type in TOKEN_WHITELIST]
-                token_count = len(tokens)
-                line_count = len(set([t.start[0] for t in tokens]))
-                table.append([
-                    relfilepath,
-                    line_count,
-                    token_count,
-                    token_count / line_count if line_count else 0,
-                ])
+            try:
+                with tokenize.open(filepath) as file:
+                    tokenized = tokenize.generate_tokens(file.readline)
+                    tokens = [t for t in tokenized if t.type in TOKEN_WHITELIST]
+                    token_count = len(tokens)
+                    line_count = len(set([t.start[0] for t in tokens]))
+                    table.append(FileData(
+                        relfilepath,
+                        line_count,
+                        token_count,
+                        token_count / line_count if line_count else 0,
+                        language_type[file_extension],
+                    ))
+            except tokenize.TokenError:
+                pass
 
-    return sorted(table, key=lambda x: -x[1])
+    return sorted(table, key=lambda x: -x.line_count)
 
 
 def main(args: List[str] = sys.argv[1:]) -> None:
@@ -47,12 +77,12 @@ def main(args: List[str] = sys.argv[1:]) -> None:
         print("Help: list <dir to give stats on>")
         sys.exit(1)
 
-    headers = ["File Name", "Lines", "Tokens", "Tokens / Line"]
+    headers = ["File Name", "Lines", "Tokens", "Tokens / Line", "Language"]
     table = gen_dim_table(args[0] if len(args) else os.getcwd())
 
-    print(tabulate([headers] + table, headers="firstrow", floatfmt=".2f"))
-    print(f"\nTotal Line Count: {sum([x[1] for x in table])}")
-    print(f"Total Token Count: {sum([x[2] for x in table])}")
+    print(tabulate([headers] + [x.expand() for x in table], headers="firstrow", floatfmt=".2f"))
+    print(f"\nTotal Line Count: {sum([x.line_count for x in table])}")
+    print(f"Total Token Count: {sum([x.token_count for x in table])}")
 
 
 if __name__ == "__main__":
